6b2de7e7f1e6ca0d997a1fda5cef0569c577cbe6
Victoria Acar <34334448+codeXORdie@users.noreply.github.com>
2018-02-22 14:30:34 +0300

    Create PlusMinus.java

---
+++ Algorithms/Warmup/PlusMinus.java
@@ -0,0 +1,28 @@
+// solution by code-xor-die
+// https://www.hackerrank.com/challenges/plus-minus/problem
+
+public class Solution {
+
+    static void plusMinus(int[] arr) {
+        int pos = 0;
+        int neg = 0;
+        int zero = 0;
+        for(int x : arr) {
+            if(x > 0) pos++;
+            if(x < 0) neg++;
+            if(x == 0) zero++;
+        }
+        System.out.printf("%.6f%n%.6f%n%.6f%n", (double)pos/arr.length, (double)neg/arr.length,    (double)zero/arr.length);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int arr_i = 0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        plusMinus(arr);
+        in.close();
+    }
+}
